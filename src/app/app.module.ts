444a8010fbcf6c4fe58b3cfec34348a3e3eb335b
Ucup Topekox <ucup.topekox@gmail.com>
2022-05-21 19:16:59 +0800

    add welcome, login, navbar component bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: "", redirectTo: "/welcome", pathMatch: "full"}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],

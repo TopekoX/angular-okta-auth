004ee66c265a21ef8c2c3bd41cb682cc5a822898
Ucup TopekoX <ucup.topekox@gmail.com>
2022-05-22 08:26:04 +0800

    update ignore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-        clientId: 'YOUR_CLIENT_ID',
+        clientId: '0oa54mt6oiarUL2LT5d7',
         issuer: 'https://YOUR_OKTA_DOMAIN/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scoper: ['openid', 'profile', 'email']

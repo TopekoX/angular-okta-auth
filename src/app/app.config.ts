0e60601533c9fd40ab4fc61bd0e0a3d881db7718
Ucup TopekoX <ucup.topekox@gmail.com>
2022-05-22 08:29:12 +0800

    update gitignore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-        clientId: '0oa54mt6oiarUL2LT5d7',
+        clientId: 'YOUR_CLIENT_ID',
         issuer: 'https://YOUR_OKTA_DOMAIN/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scoper: ['openid', 'profile', 'email']
